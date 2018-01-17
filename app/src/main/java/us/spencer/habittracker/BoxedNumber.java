6eb3eda6928e3136402ecf640fd2684f1c8d5e03
Spencer Daniel <spencerdaniel93@gmail.com>
2018-01-16 19:12:38 -0600

    Added attr file for new custom view. Also generalizing the size calculations so it can be reused.

--- app/src/main/java/us/spencer/habittracker/BoxedNumber.java
+++ app/src/main/java/us/spencer/habittracker/BoxedNumber.java
@@ -1,6 +1,7 @@
 package us.spencer.habittracker;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,19 +15,29 @@ import android.view.View;
 
 public class BoxedNumber extends View {
 
-    private static final float BOX_WIDTH = 40.0f;
+    private static final String DAY_OF_MONTH_REGEX = "DD";
 
-    private static final float BOX_HEIGHT = 40.0f;
+    private static final float DEFAULT_TEXT_SIZE = 15.0f;
+
+    private static final boolean DEFAULT_BOLD_VALUE = false;
+
+    private static final int DEFAULT_MARGIN_VALUE = 5;
+
+    private String mText;
+
+    private float mTextSize;
+
+    private boolean mIsBold;
+
+    private int mMargin;
 
     private Paint mBoxPaint = new Paint();
 
     private Paint mNumberPaint = new Paint();
 
-    private RectF mBox = new RectF(0.0f, BOX_HEIGHT, BOX_WIDTH, 0.0f);
+    private RectF mBox = new RectF();
 
     private Rect r  = new Rect();
-
-    private String mText = "DD";
 
     public BoxedNumber(Context context) {
         super(context);
@@ -35,12 +46,29 @@ public class BoxedNumber extends View {
 
     public BoxedNumber(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        setupAttributes(attrs);
         init();
     }
 
     public BoxedNumber(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setupAttributes(attrs);
         init();
+    }
+
+    private void setupAttributes(AttributeSet attrs) {
+        TypedArray attr = getContext().getTheme().obtainStyledAttributes(attrs,
+                R.styleable.BoxedNumber, 0, 0);
+
+        try {
+            mText = attr.getString(R.styleable.BoxedNumber_bn_text);
+            mTextSize = attr.getFloat(R.styleable.BoxedNumber_bn_text_size, DEFAULT_TEXT_SIZE);
+            mIsBold = attr.getBoolean(R.styleable.BoxedNumber_bn_text_bold, DEFAULT_BOLD_VALUE);
+            mMargin = attr.getInteger(R.styleable.BoxedNumber_bn_margin, DEFAULT_MARGIN_VALUE);
+        }
+        finally {
+            attr.recycle();
+        }
     }
 
     private void init() {
@@ -48,13 +76,33 @@ public class BoxedNumber extends View {
 
         mNumberPaint.setColor(Color.WHITE);
         mNumberPaint.setAntiAlias(true);
-        mNumberPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        mNumberPaint.setTextSize(25.0f);
+        if(mIsBold) {
+            mNumberPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        }
+        else {
+            mNumberPaint.setTypeface(Typeface.DEFAULT);
+        }
+        mNumberPaint.setTextSize(mTextSize);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mNumberPaint.getTextBounds(DAY_OF_MONTH_REGEX, 0,
+                DAY_OF_MONTH_REGEX.length(), r); /* Displays only days of month */
+
+        int contentWidth = (r.width() + mMargin) * 2;
+        int w = resolveSize(contentWidth + getPaddingLeft() + getPaddingRight(),
+                widthMeasureSpec);
+
+        int contentHeight = (r.height() + mMargin) * 2;
+        int h = resolveSize(contentHeight + getPaddingTop() + getPaddingBottom(),
+                heightMeasureSpec);
+
+        int boxSize = h < w ? w : h;
+        mBox.top = boxSize - mMargin;
+        mBox.right = boxSize - mMargin;
+
+        setMeasuredDimension(boxSize, boxSize);
     }
 
     @Override
@@ -66,8 +114,8 @@ public class BoxedNumber extends View {
 
     private void drawTextCenter(Canvas canvas) {
         canvas.getClipBounds(r);
-        int cHeight = r.height();
-        int cWidth = r.width();
+        int cHeight = r.height() - mMargin;
+        int cWidth = r.width() - mMargin;
         mNumberPaint.setTextAlign(Paint.Align.LEFT);
         mNumberPaint.getTextBounds(mText, 0, mText.length(), r);
         float x = cWidth / 2f - r.width() / 2f - r.left;
@@ -77,11 +125,9 @@ public class BoxedNumber extends View {
 
     public void setText(String text) {
         mText = text;
-        invalidate();
     }
 
     public void setBoxColor(int color) {
         mBoxPaint.setColor(color);
-        invalidate();
     }
 }
